c6fbdbb0bb24850ca84d0b4d06c70ae9df2cf230
jmavlonov <jasurmavloniy24@gmail.com>
2025-11-07 20:33:46 +0500

    done

---
+++ app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .models import Category , Product
+from django.http import JsonResponse
+# Create your views here.
+
+
+def index(request,category_id = None):
+    
+    categories = Category.objects.all()
+    
+    if category_id:
+        products = Product.objects.filter(category = category_id)
+    else:
+        products = Product.objects.all()
+    
+    
+    context = {
+        'categories':categories,
+        'products':products
+    }
+    return render(request,'app/home.html',context)
+
+
+
+def detail(request,product_id):
+    product = Product.objects.get(id = product_id)
+    if not product:
+        return JsonResponse(data={'message':'Oops. Page Not Found','status_code':404})
+    
+    context = {
+        'product' : product
+    }
+    return render(request,'app/detail.html',context)
+
